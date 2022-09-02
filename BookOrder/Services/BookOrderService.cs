b6fcb4fae56e251906433941f62cd6aeb02094dd
Othman <othkimweri@gmail.com>
2022-09-02 18:36:44 +0300

    added services folder to project

---
+++ BookOrder/Services/BookOrderService.cs
@@ -0,0 +1,27 @@
+﻿using BookOrder.Models;
+
+namespace BookOrder.Services
+{
+    public class BookOrderService : IBookOrderService
+    {
+        public Book Add(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Book> GetAllBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Book book)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BookOrder/Services/IBookOrderService.cs
@@ -0,0 +1,12 @@
+﻿using BookOrder.Models;
+
+namespace BookOrder.Services
+{
+    internal interface IBookOrderService
+    {
+        IEnumerable<Book> GetAllBooks();
+        Book Add(Book book);
+        Book GetById(int id);
+        void Remove(Book book);
+    }
+}
