4d32c9ed16ced60c53732fa7484555fad042d2a6
Othman <othkimweri@gmail.com>
2022-09-03 09:32:16 +0300

    implemented members checkout in concrete class that implements book order service

--- BookOrder/Services/BookOrderService.cs
+++ BookOrder/Services/BookOrderService.cs
@@ -4,24 +4,21 @@ namespace BookOrder.Services
 {
     public class BookOrderService : IBookOrderService
     {
-        public Book Add(Book book)
+
+        public void Checkout(Order bookOrder)
         {
             throw new NotImplementedException();
         }
 
+        public Book GetBook(int id)
+        {
+            throw new NotImplementedException();
+        }
         public IEnumerable<Book> GetAllBooks()
         {
             throw new NotImplementedException();
         }
 
-        public Book GetById(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Remove(Book book)
-        {
-            throw new NotImplementedException();
-        }
+      
     }
 }
