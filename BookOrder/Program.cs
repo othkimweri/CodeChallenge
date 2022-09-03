eb9ce6c8ceb1c85f4e110682ba99231dda387c1b
Othman <othkimweri@gmail.com>
2022-09-03 09:34:37 +0300

    injected dependency book order service in program.cs

--- BookOrder/Program.cs
+++ BookOrder/Program.cs
@@ -1,3 +1,5 @@
+using BookOrder.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IBookOrderService, BookOrderService>();
 
 var app = builder.Build();
 
@@ -15,7 +18,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseDefaultFiles();
+app.UseStaticFiles();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
