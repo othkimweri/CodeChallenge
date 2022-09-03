03547f732be33567848c99fc8379f496f618d4c5
Othman <othkimweri@gmail.com>
2022-09-03 09:28:03 +0300

    changed base route from /controller to api/controller

--- BookOrder/Controllers/BookOrderController.cs
+++ BookOrder/Controllers/BookOrderController.cs
@@ -1,33 +1,64 @@
 using BookOrder.Services;
 using Microsoft.AspNetCore.Mvc;
+using BookOrder.Models;
 
 namespace BookOrder.Controllers;
 
 [ApiController]
-[Route("[controller]")]
-public class BookController : ControllerBase
+[Route("api/[controller]")]
+public class BookOrderController : ControllerBase
 {
     private readonly IBookOrderService _bookOrderService;
+    private readonly List<Book> _bookList;
 
-	public BookController(IBookOrderService bookOrderService)
+    public BookOrderController(IBookOrderService bookOrderService)
 	{
 		_bookOrderService = bookOrderService;
-	}
+        _bookList = new List<Book>()
+            {
+                new Book()
+                {
+                       Id = 1, Title = "English Grammar", Price = 25000, Level = "Senior One"
+                },
+                  new Book()
+                {
+                       Id = 5, Title = "English Grammar Teacher's Edition", Price = 20000, Level = "Senior One"
+                },
+                  new Book()
+                {
+                       Id = 2, Title = "Kiswahili", Price = 25000, Level = "Senior Two"
+                },
+                    new Book()
+                {
+                       Id = 3, Title = "Mathematics", Price = 25000, Level = "Teacher Edition"
+                },
+
+            };
+    }
 	[HttpGet]
 	public IActionResult Get()
 	{
-		var books = _bookOrderService.GetAllBooks();
-		return Ok(books);
+		//var books = _bookOrderService.GetAllBooks();
+		return Ok(_bookList);
 	}
 
-    [HttpPost]
-    public IActionResult Get(BookOrder bookOrder)
+    [HttpGet]
+    public IActionResult Get(int id)
     {
-        var book = _bookOrderService.GetById(id);
-        if (book == null)
-        {
-            return NotFound();
-        }
+        //var books = _bookOrderService.GetAllBooks();
+        var book = _bookList.Where(b => b.Id == id).FirstOrDefault();
         return Ok(book);
+    }
+
+    [HttpPost]
+    public IActionResult Checkout([FromBody] Order bookOrder)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        //TODO:Email the order asynchronously
+        string confirmationMessage = "An email with your order has been sent to your email";
+        return Ok(confirmationMessage);
     }
 }
